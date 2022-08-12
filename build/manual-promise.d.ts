911c386fcfcefe3175fc498f65679a3befb3edcd
Zim <zimtsui@gmail.com>
2022-08-12 17:02:08 +0800

    promiselike to promise

--- build/manual-promise.d.ts
+++ build/manual-promise.d.ts
@@ -3,7 +3,7 @@ export declare class ManualPromise<T> implements PromiseLike<T> {
     resolve: (value: T | PromiseLike<T>) => void;
     reject: (reason?: any) => void;
     constructor();
-    then<TResult1 = T, TResult2 = never>(onFulfilled: ((value: T) => TResult1 | PromiseLike<TResult1>) | null | undefined, onRejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | null | undefined): PromiseLike<TResult1 | TResult2>;
-    catch<TResult2>(onRejected: ((reason: any) => TResult2 | PromiseLike<TResult2>) | null | undefined): PromiseLike<T | TResult2>;
-    finally(onFinally: () => void): PromiseLike<T>;
+    then<TResult1 = T, TResult2 = never>(onFulfilled: ((value: T) => TResult1 | PromiseLike<TResult1>) | null | undefined, onRejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | null | undefined): Promise<TResult1 | TResult2>;
+    catch<TResult2>(onRejected: ((reason: any) => TResult2 | PromiseLike<TResult2>) | null | undefined): Promise<T | TResult2>;
+    finally(onFinally: () => void): Promise<T>;
 }
--- src/manual-promise.ts
+++ src/manual-promise.ts
@@ -1,5 +1,5 @@
 export class ManualPromise<T> implements PromiseLike<T> {
-	private native: PromiseLike<T>;
+	private native: Promise<T>;
 	public resolve!: (value: T | PromiseLike<T>) => void;
 	public reject!: (reason?: any) => void;
 
@@ -11,17 +11,20 @@ export class ManualPromise<T> implements PromiseLike<T> {
 		this.native.then(x => x, () => { });
 	}
 
-	public then<TResult1 = T, TResult2 = never>(onFulfilled: ((value: T) => TResult1 | PromiseLike<TResult1>) | null | undefined, onRejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | null | undefined): PromiseLike<TResult1 | TResult2> {
+	public then<TResult1 = T, TResult2 = never>(
+		onFulfilled: ((value: T) => TResult1 | PromiseLike<TResult1>) | null | undefined,
+		onRejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | null | undefined,
+	): Promise<TResult1 | TResult2> {
 		return this.native.then(onFulfilled, onRejected);
 	}
 
 	public catch<TResult2>(
 		onRejected: ((reason: any) => TResult2 | PromiseLike<TResult2>) | null | undefined,
-	): PromiseLike<T | TResult2> {
+	): Promise<T | TResult2> {
 		return this.native.then(x => x, onRejected);
 	}
 
-	public finally(onFinally: () => void): PromiseLike<T> {
+	public finally(onFinally: () => void): Promise<T> {
 		return this.then(onFinally, onFinally)
 			.then(() => this);
 	}
